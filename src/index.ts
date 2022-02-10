f8e5b500c9c56808280684022c1b3ce02d51651a
Noer Paanakker <noer@lightworksweb.nl>
2022-02-10 23:51:26 +0100

    finished db setup

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { MikroORM } from "@mikro-orm/core";
 import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init(mikroOrmConfig);
-  console.log("orm", orm);
+  const orm = await MikroORM.init(mikroOrmConfig); // Connect to DB
+  await orm.getMigrator().up(); // Run migration to sync data in  DB
 };
 
 main().catch((err) => console.error(err));

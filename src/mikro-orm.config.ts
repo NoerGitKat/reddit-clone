9870e200b7804ed7b311da9dbb1fe5b3a469de9b
Noer Paanakker <noer@lightworksweb.nl>
2022-02-12 10:22:14 +0100

    setup migrations config correctly now

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { __prod__ } from "./constants";
 import Post from "./models/Post";
 import { MikroORM } from "@mikro-orm/core";
+// import User from "./models/User";
 
 export default {
   migrations: {
--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -21,11 +21,39 @@ export default class PostResolver {
   }
 
   @Query(() => Post, { nullable: true })
-  getPostById(
+  getPost(
     @Arg("id", () => Int) id: number,
     @Ctx()
     { em }: PostContext,
   ): Promise<Post | null> {
     return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id", () => Number) id: number,
+    @Arg("title", () => String, { nullable: true }) title: string,
+    @Ctx() { em }: PostContext,
+  ): Promise<Post | null> {
+    const fetchedPost = await em.findOne(Post, { id });
+    if (!fetchedPost) return null;
+    if (typeof title !== "undefined") {
+      fetchedPost.title = title;
+      await em.persistAndFlush(fetchedPost);
+    }
+    return fetchedPost;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(@Arg("id") id: number, @Ctx() { em }: PostContext): Promise<boolean> {
+    try {
+      const fetchedPost = await em.findOne(Post, { id });
+      if (!fetchedPost) return false;
+      await em.nativeDelete(Post, { id });
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
   }
 }
